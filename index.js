e20af9540f9d8eed540497da9193055f1f60e1b1
Daniel Hall <daniel.hall@lifx.com>
2019-11-18 14:31:49 +1100

    Don't use the stringified version

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 
-  core.setOutput("conclusion", payload.check_run.conclusion);
+  core.setOutput("conclusion", github.context.payload.check_run.conclusion);
 } catch (error) {
   core.setFailed(error.message);
 }
